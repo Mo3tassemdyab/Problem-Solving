62bf9ee7448a3a3a086fa621e9aacb04e025c085
Moatasem Dyab <105018563+Mo3tassemdyab@users.noreply.github.com>
2024-11-18 13:36:10 +0200

    Add files via upload

---
+++ MakeUpperCase/main.js
@@ -0,0 +1,7 @@
+// Description:
+// Write a function which converts the input string to upperCase
+
+function upperCase(str){
+    return str.toUpperCase();
+}
+console.log(upperCase('hello moatasem'));
