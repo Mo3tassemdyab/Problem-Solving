c9b63ee802aa3841deb8906792337289f838eb71
Moatasem Dyab <105018563+Mo3tassemdyab@users.noreply.github.com>
2024-11-18 14:27:20 +0200

    first-commit

---
+++ get_opposite/main.js
@@ -0,0 +1,9 @@
+// very simple, given an integer or a floating-point number, find its opposite
+
+function getOpposite(num){
+  // return num * -1;
+  return - num;
+}
+
+console.log(getOpposite(5));
+
